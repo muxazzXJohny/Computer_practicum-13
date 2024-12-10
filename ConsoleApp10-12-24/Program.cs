5909f9b5c7ee037895cb333c545793bf40ef2a5a
muxazzXJohny <maracoco334@gmail.com>
2024-12-10 09:35:07 +0200

    Add files via upload

---
+++ ConsoleApp10-12-24/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10_12_24
+{
+    class Program
+    {
+        /// <summary>
+        /// Дано целое число n, указывающее номер дня недели от 1 до 7. По указанному номеру n вывести название соответствующего дня недели.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            int day = Input("Введите число недели: ");
+            Week(day);
+            Console.ReadKey();       
+        }
+        static string GetName(int day)
+
+        {
+            switch (day)
+            {
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+                default:
+                    Console.WriteLine("не верное число недели");
+                    break;
+            }
+            static int Input(string day)
+            {
+                Console.Write(day);
+                return int.Parse(Console.ReadLine());
+
+            }
+
+
