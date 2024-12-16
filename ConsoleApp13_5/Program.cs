a168910ed6ccf628f2ad9236625692d6363ac4ff
muxazzXJohny <maracoco334@gmail.com>
2024-12-16 19:10:20 +0200

    Add files via upload

---
+++ ConsoleApp13_5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13_5
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Value();
+            Console.ReadKey();
+        }
+        public static void Value()
+        {
+            string n = Input("Введите букву ");
+            Console.WriteLine(GetName(n));
+            Console.ReadKey();
+        }
+        public static string Input(string message)
+        {
+            Console.Write(message);
+            return Console.ReadLine();
+        }
+        public static string GetName(string letter)
+        {
+            switch (letter.ToLower())
+            {
+                case "а": return "автомобиль максимальная скорость 508,73 км/ч.";
+                case "в": return "велосипед максимальная скорость 295,6 км/ч";
+                case "м": return "мотоцикл максимальная скорость 200 км/ч.";
+                case "с": return "самолёт максимальная скорость 8200,8 км/ч.";
+                case "п": return "поезд максимальная скорость 574,8 км/ч.";
+                default: return "Введена неправильная буква";
+            }
+        }
+    }
+}
