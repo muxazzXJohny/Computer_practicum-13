8ad96383a8a03657234f8f08b44db3870d074a69
muxazzXJohny <maracoco334@gmail.com>
2024-12-16 19:09:15 +0200

    Add files via upload

---
+++ ConsoleApp13-3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Input("Введите число месяца: ");
+            int Month = (number);
+        }
+        static string GetName(int Month)
+        {
+            switch (Month)
+            {
+                case 1: case 2: case 12: return ("Зима");
+                case 3: case 4: case 5: return ("Весна");
+                case 6: case 7: case 8: return ("Лето");
+                case 9: case 10: case 11: return ("Зима");
+                default:
+                    return ("Такого месяца нету");
+
+            }
+            }
+            static int Input(string a)
+            {
+                Console.Write("a=");
+                return int.Parse(Console.ReadLine());
+            }
+
+
+        
+    }
+}
